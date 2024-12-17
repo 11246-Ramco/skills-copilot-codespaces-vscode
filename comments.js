f6a2cc2a120103d6206bf71bfcd16478aa63b428
11246-Ramco <hariharasudhanshanmugasundaram@hotmail.com>
2024-12-17 10:59:54 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,4 @@
 //create web server
-import express from 'express';
+const express = require('express');
+const { comments } = require('./data/comments');
 const app = express();
-
-//create a route
-app.get('/comments', (req, res) => {
-  res.send('This is the comments page');
-});
-
-//start the server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
