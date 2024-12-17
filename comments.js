ea97d6f965bee51408f214ea4f39d8cd4dbe3807
11246-Ramco <hariharasudhanshanmugasundaram@hotmail.com>
2024-12-17 10:49:18 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,43 +1,13 @@
 //create web server
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-//create a comment
-app.post('/comments', function (req, res) {
-    const comment = req.body.comment;
-    if (!comment) {
-        return res.status(400).send('Missing comment');
-    }
-    fs.readFile('comments.json', 'utf8', function (err, data) {
-        if (err) {
-            return res.status(500).send('Error reading comments file');
-        }
-        const comments = JSON.parse(data);
-        comments.push(comment);
-        fs.writeFile('comments.json', JSON.stringify(comments), function (err) {
-            if (err) {
-                return res.status(500).send('Error writing comments file');
-            }
-            res.send('Comment added');
-        });
-    });
+//create a route
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page');
 });
 
-//get all comments
-app.get('/comments', function (req, res) {
-    fs.readFile('comments.json', 'utf8', function (err, data) {
-        if (err) {
-            return res.status(500).send('Error reading comments file');
-        }
-        const comments = JSON.parse(data);
-        res.send(comments);
-    });
-});
-
-app.listen(3000, function () {
-    console.log('Server is listening on port 3000');
+//start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
